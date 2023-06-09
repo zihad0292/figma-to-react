a72f81174ef5e01238b90b2d9c46f6479c05c552
Zihad Ul Islam <zihad@scbd.com>
2023-06-10 01:33:34 +0600

    Project initialization with GitHub repo

---
+++ src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  const texts = ["Hello", "Bonjour", "Hola", "Ciao"];
+  const interval = 2000; // 2 seconds
+  return (
+    <div>
+      <h2 className="text-3xl font-bold underline">Figma to react</h2>
+    </div>
+  );
+}
+
+export default App;
+
